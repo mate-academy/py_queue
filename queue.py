b884457fa70927fbe3e8436d512146cfa7346883
logotip <logotip123@yahoo.com>
2019-11-25 12:26:38 +0200

    v 1.0

--- queue.py
+++ queue.py
@@ -1,10 +1,25 @@
+"""class FIFO module"""
+
+
 class Queue:
-    def __init__(self, length: int):
-        ...
+    """class FIFO"""
+    def __init__(self, length=10):
+        self.array = []
+        self.maxlength = length
 
     def push(self, value: int):
-        ...
+        """
+        Function for check array length and push new element.
+        :param value: new element
+        :return: None
+        """
+        if len(self.array) >= self.maxlength:
+            raise IndexError
+        self.array.append(value)
 
     def pop(self) -> int:
-
-        return 0
+        """
+        function for remove first element from array
+        :return: removed element
+        """
+        return self.array.pop(0)
