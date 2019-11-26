02f8d9724dff3837b7c452ec50b4286d1ff93129
Andrii <andrewsotnikov88@gmail.com>
2019-11-26 16:18:16 +0200

    FIFO queue implementation

--- queue.py
+++ queue.py
@@ -1,10 +1,19 @@
+"""Implement FIFO queue according to the given interface. Queue must have a limited length."""
+from typing import List
+
 class Queue:
-    def __init__(self, length: int):
-        ...
+    """FIFO queue implementation"""
+    def __init__(self, length: int = 0):
+        """Docstring init"""
+        self.array: List[int] = []
+        self.length = length
 
     def push(self, value: int):
-        ...
+        """Pushing value to the end of array"""
+        if self.length and self.length <= len(self.array):
+            raise IndexError
+        self.array.append(value)
 
     def pop(self) -> int:
-
-        return 0
+        """Method removes the last element from an array"""
+        return self.array.pop(0)
