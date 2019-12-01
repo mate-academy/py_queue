a1b73481423956d98f2527dc34ea21fdd8a4766f
koreyo <KoretsValentyn@gmail.com>
2019-12-01 12:45:07 +0200

    queue

--- queue.py
+++ queue.py
@@ -1,10 +1,18 @@
+"""docstring"""
+
 class Queue:
-    def __init__(self, length: int):
-        ...
+    """init queue structure"""
+    def __init__(self, length=None):
+        """init"""
+        self.items = []
+        self.length = length
 
     def push(self, value: int):
-        ...
+        """add new element"""
+        if self.length and len(self.items) == self.length:
+            raise IndexError
+        self.items.append(value)
 
     def pop(self) -> int:
-
-        return 0
+        """return first element"""
+        return self.items.pop(0)
